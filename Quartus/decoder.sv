fca4faa23b8ca60ab8f4691e11f877836be5bf88
Torino1564 <jtorinor6@gmail.com>
2026-03-28 15:51:18 -0300

    Version funcional del decoder

--- Quartus/decoder.sv
+++ Quartus/decoder.sv
@@ -4,11 +4,11 @@ module decoder
 	input wire [31:0] inst,
 	output reg [4:0] rs1,
 	output reg [4:0] rs2,
+	output reg [4:0] rd,
 	output wire [6:0] opcode,
 	output reg [2:0] func3,
 	output reg [6:0] func7,
-	output reg [31:0] imm,
-	output reg [2:0] inst_type
+	output reg [31:0] imm
 );
 	// Define instruction types
 	enum bit[2:0] {R, I, S, B, U, J, E} 	INST_TYPE;
@@ -25,9 +25,92 @@ module decoder
 			7'b0100011: inst_type = S;	// STORE
 			7'b1100011: inst_type = B;	// BRANCH
 			7'b1101111: inst_type = J;	// JAL
-			7'b0100011: inst_type = U;	// LUI
+			7'b0110111: inst_type = U;	// LUI
 			7'b0010111: inst_type = U;	// AUIPC
 			default:		inst_type = E; // ERROR
+		endcase
+	end
+	
+	// Assign outputs
+	
+	// RS1 & Func3
+	always @(*) begin
+		case (inst_type)
+			R, I, S, B: begin 
+				rs1 = inst[19:15];
+				func3 = inst[14:12];
+			end
+			default: begin
+				func3 = func3;
+				rs1 = rs1;
+			end
+		endcase
+	end
+	
+	// RS2
+	always @(*) begin
+		case (inst_type)
+			R, S, B: begin 
+				rs2 = inst[24:20];
+			end
+			default: begin
+				rs2 = rs2;
+			end
+		endcase
+	end
+	
+	// RD
+	always @(*) begin
+		case (inst_type)
+			R, I, U, J: begin 
+				rd = inst[11:7];
+			end
+			default: begin
+				rd = rd;
+			end
+		endcase
+	end
+	
+	// Func7
+	always @(*) begin
+		case (inst_type)
+			R: begin 
+				func7 = inst[31:25];
+			end
+			default: begin
+				func7 = func7;
+			end
+		endcase
+	end
+	
+	// Imm
+	always @(*) begin
+		case (inst_type)
+			I: begin 
+				imm[11:0] = inst[31:20];
+			end
+			S: begin 
+				imm[11:5] = inst[31:25];
+				imm[4:0] = inst[11:7];
+			end
+			B: begin 
+				imm[12] = inst[31];
+				imm[10:5] = inst[30:25];
+				imm[4:1] = inst[11:8];
+				imm[11] = inst[7];
+			end
+			U: begin 
+				imm[31:12] = inst[31:12];
+			end
+			J: begin 
+				imm[20] = inst[31];
+				imm[10:1] = inst[30:21];
+				imm[11] = inst[20];
+				imm[19:12] = inst[19:12];
+			end
+			default: begin
+				imm = imm;
+			end
 		endcase
 	end
 	
--- Quartus/decoder_tb.sv
+++ Quartus/decoder_tb.sv
@@ -1,34 +1,36 @@
 `timescale 1ns/1ps
 
 module decoder_tb;
-	
-	reg clk = 0;
-	always #5 clk = ~clk;
-	
+
 	reg [31:0] inst = 32'b0;
 
 	initial begin
-		inst[6:0] = 7'b0000011;
+		inst[6:0] = 7'b1101111; // opcode
+		inst[19:15] = 5'b10101; // rs1
+		inst[24:20] = 5'b11001; // rs2
+		inst[11:7] = 5'b01110; // rd
+		inst[14:12] = 3'b001; // func3
+		inst[31:25] = 7'b1011011; // func7
 	end
 	
 	wire [4:0] rs1;
 	wire [4:0] rs2;
+	wire [4:0] rd;
 	wire [6:0] opcode;
 	wire [2:0] func3;
 	wire [6:0] func7;
 	wire [31:0] imm;
-	wire [2:0] inst_type;
 	
 	decoder dut (
 		.clk(clk),
 		.inst(inst),
 		.rs1(rs1),
 		.rs2(rs2),
+		.rd(rd),
 		.opcode(opcode),
 		.func3(func3),
 		.func7(func7),
-		.imm(imm),
-		.inst_type(inst_type)
+		.imm(imm)
 	);
 	
 endmodule
