cec0d3ba7a579a7cb80fc4533192552dfbdb4db5
Torino1564 <jtorinor6@gmail.com>
2026-03-30 15:17:44 -0300

    addrBuilder

--- Quartus/addrBuilder_tb.sv
+++ Quartus/addrBuilder_tb.sv
@@ -1,24 +1,35 @@
 module addrBuilder_tb;
 
-	wire[31:0] rs1data = 'z;
-	wire[31:0] rs2data = 'z;
-	wire[31:0] pc = 'b0;
-	wire[31:0] imm = 'z;
-	wire[31:0] opcode = 'z;
-	wire stageEna = 'z;
-	wire stageX = 'z;
+	reg[31:0] rs1data = 'z;
+	reg[31:0] rs2data = 'z;
+	reg[31:0] pc = 'b0;
+	reg[31:0] imm = 'z;
+	reg[31:0] opcode = 'z;
+	reg stageEna = 'z;
+	reg stageX = 'z;
+	
+	wire [31:0] dataAdd;
 
-initial begin
-	rs1data = 4'b1100;
-	imm = 4'b0011;
-	stageEna = 'b1;
-end
+	initial begin
+		rs1data = 4'b1100;
+		imm = 4'b0011;
+		stageEna = 'b1;
+		
+		#20 
+		stageEna = 0;
+	end
 
-addrBuilder addrBuilder_inst
-(
-.rs1data(rs1data),
-.imm(imm),
-);
+	addrBuilder addrBuilder_inst
+	(
+		.rs1data(rs1data),
+		.rs2data(rs2data),
+		.opcode(opcode),
+		.pc(pc),
+		.imm(imm),
+		.stageEna(stageEna),
+		.stageX(stageX),
+		.dataAdd(dataAdd)
+	);
 
 endmodule
 	
